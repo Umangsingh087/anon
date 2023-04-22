233ec7380905386af79c3f17b6cfcc7afc40054d
Umangsingh087 <117168866+Umangsingh087@users.noreply.github.com>
2023-04-22 09:53:58 +0530

    Update start.py
    Signed-off-by: Umangsingh087 <117168866+Umangsingh087@users.noreply.github.com>

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -40,9 +40,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             )
         ],
         [
-            InlineKeyboardButton(
-                text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
-            ),
+            
             InlineKeyboardButton(
                 text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
             )
